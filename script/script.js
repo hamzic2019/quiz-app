ae52d385a163308d4ea0d054c2667e4abddcdeb5
Haris Hamzic <hamzicba@icloud.com>
2020-08-17 21:51:23 +0200

    more js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 const forma = document.querySelector('.quiz-form');
+const displayScoreText = document.querySelector('#display-score');
+const scored = document.querySelector('#scored');
 
 let answers = ['B', 'A', 'B']
 let correctAnswers = [];
-
+let score = 0;
 
 forma.addEventListener('submit', e => {
     e.preventDefault();
@@ -12,8 +14,24 @@ forma.addEventListener('submit', e => {
     correctAnswers.push(forma.trd.value)
 
     correctAnswers.forEach((answer, index) => {
-
+        if(answer == answers[index]) return score += 1;
     });
 
+    displayScoreText.classList.toggle('visibility-block');
+    scrollTo(0,0);
+
     correctAnswers = [];
+    
+    displayScore(score);
+    score = 0;
 });
+
+
+const displayScore = (score) => {
+    if(score == 1) return scored.innerText = `33.3%`;
+    if (score == 2) return scored.innerText = `66.6%`;
+    if(score == 3) return scored.innerText = `100%`;
+
+}
+
+
